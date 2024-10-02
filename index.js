2cb20e18951aa08e3e95970815967a73e7285249
Ashlyn Carter <eannacarter@gmail.com>
2024-10-02 10:27:49 -0400

    There's nothing that's really worth writing here, but it makes me write something. so here.

---
+++ index.js
@@ -0,0 +1,24 @@
+//NOTE: Git hub decided to be a jerk today, so I didn't get
+//      much time for this unfortunately.
+/*
+Object: Cat
+
+Properties: 
+    Color
+    Pattern
+    Fur length
+    Eye color
+    Number of legs
+    Has teeth (t/f)
+    Feral/domesticated
+Events:
+    Meow
+    Eat food
+    play with toys
+    drink water
+Methods: 
+    make meow sound
+    decrease hunger
+    decrease boredom
+    decrease thirst
+*/
